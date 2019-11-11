07ac7d37f9123eb15b65a4a0648cf8124e0b860f
Joe Innes <joe@joeinn.es>
2019-11-11 22:43:24 +0000

    Began work on editor

---
+++ components/Editor.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <section v-if="type === 'link'">
+      <b-field label="Link">
+        <b-input v-model="linkText" type="url"></b-input>
+      </b-field>
+    </section>
+    <section v-if="type === 'song'">
+      <b-field label="Song">
+        <b-input v-model="linkText" type="url"></b-input>
+      </b-field>
+    </section>
+    <section v-if="type === 'video'">
+      <b-field label="Video">
+        <b-input v-model="linkText" type="url"></b-input>
+      </b-field>
+    </section>
+    <b-field label="Note">
+      <b-input v-model="note" type="textarea"></b-input>
+    </b-field>
+    <b-field label="Add some tags">
+      <b-taginput
+        v-model="tags"
+        ellipsis
+        icon="label"
+        placeholder="Add a tag"
+        autocomplete
+        allow-new
+      >
+      </b-taginput>
+    </b-field>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      linkText: '',
+      note: '',
+      tags: []
+    }
+  }
+}
+</script>
