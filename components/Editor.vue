78c7ff7851d7e1cefd44f2557527f5eb8115922a
Joe Innes <joe@joeinn.es>
2019-11-11 23:03:46 +0000

    Added collapsible additional options for editor

--- components/Editor.vue
+++ components/Editor.vue
@@ -15,20 +15,31 @@
         <b-input v-model="linkText" type="url"></b-input>
       </b-field>
     </section>
+    <b-field label="Title">
+      <b-input v-model="title"></b-input>
+    </b-field>
     <b-field label="Note">
       <b-input v-model="note" type="textarea"></b-input>
     </b-field>
-    <b-field label="Add some tags">
-      <b-taginput
-        v-model="tags"
-        ellipsis
-        icon="label"
-        placeholder="Add a tag"
-        autocomplete
-        allow-new
-      >
-      </b-taginput>
-    </b-field>
+    <b-collapse :open="false">
+      <div slot="trigger" slot-scope="props" role="button">
+        <b-icon :icon="props.open ? 'menu-up' : 'menu-down'"></b-icon>
+      </div>
+      <b-field label="Add some tags">
+        <b-taginput
+          v-model="tags"
+          ellipsis
+          icon="label"
+          placeholder="Add a tag"
+          autocomplete
+          allow-new
+        >
+        </b-taginput>
+      </b-field>
+      <b-field label="Source">
+        <b-input v-model="source" type="url"></b-input>
+      </b-field>
+    </b-collapse>
   </section>
 </template>
 
@@ -44,7 +55,9 @@ export default {
     return {
       linkText: '',
       note: '',
-      tags: []
+      title: '',
+      tags: [],
+      source: ''
     }
   }
 }
